506b86f9639a0f417603a6d5cf60bba5e369a9a4
PjHurtig <>
2023-08-10 11:39:40 +0000

    navbar form challenge

---
+++ my-app/src/components/EventsClass.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class EventsClass extends Component {
+  render() {
+    return (
+      <div>EventsClass</div>
+    )
+  }
+}
+
+export default EventsClass
---
+++ my-app/src/components/EventsFunctional.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EventsFunctional() {
+  return (
+    <div>EventsFunctional</div>
+  )
+}
+
+export default EventsFunctional
